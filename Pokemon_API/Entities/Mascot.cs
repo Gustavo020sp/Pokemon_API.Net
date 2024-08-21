46036396fc9e63dcc39e080364814159eeb6f198
Gustavo Gomes <gustavo020sp@gmail.com>
2024-08-21 13:57:31 -0300

    Criado uma classe que representa um mascote e colocado os atributos com o mesmo nome e tipo de dados dos campos retornados no JSON; Criado uma classe Wrapper para representar a estrutura array do json, e uma classe ability para pegar o nome da ability resultado da API convertido neste objeto criado;

---
+++ Pokemon_API/Entities/Mascot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace Pokemon_API.Entities
+{
+    public class Mascot
+    {
+        public string? name { get; set; }
+        public int height { get; set; }
+        public int weight { get; set; }
+
+        public List<AbilityWrapper>? abilities { get; set; }
+
+        public override string ToString()
+        {
+            string abilityname = "";
+
+            foreach (var abilityWrapper in abilities)
+            {
+                abilityname += "- " + abilityWrapper.Ability.name + Environment.NewLine;
+            }
+
+            return Environment.NewLine + "Pokemon Name: " + name + Environment.NewLine
+                + "Height: " + height + Environment.NewLine
+                + "Weight: " + weight + Environment.NewLine
+                + "Abilities: " + Environment.NewLine
+                + abilityname;
+        }
+
+    }
+}
--- Pokemon_API/Program.cs
+++ Pokemon_API/Program.cs
@@ -1,5 +1,8 @@
 ﻿using RestSharp;
 using static System.Net.WebRequestMethods;
+using System.Text.Json;
+using Pokemon_API.Entities;
+using Microsoft.VisualBasic;
 
 namespace Pokemon_API
 {
@@ -9,15 +12,16 @@ namespace Pokemon_API
         {
             int response = 0;
             string pokemon_url = "";
+
             do
             {
-                Console.WriteLine("Opções de pokémon:");
-                Console.WriteLine("Bulbasaur [1]" + Environment.NewLine + "Ivysaur [2]" + Environment.NewLine + "Venusaur [3]");
-                Console.Write("Digite o número do pokémon que deseja > ");
+                Console.WriteLine("Pokémon options:");
+                Console.WriteLine("Bulbasaur [1]" + Environment.NewLine + "Ivysaur [2]" + Environment.NewLine + "Venusaur [3]" + Environment.NewLine + "Charmander [4]");
+                Console.Write("Enter a pokémon number > ");
 
                 response = Convert.ToInt32(Console.ReadLine());
 
-                if (response > 3)
+                if (response == 0 || response > 4)
                 {
                     Console.WriteLine();
                     Console.WriteLine("Choose a valid pokémon number." + Environment.NewLine);
@@ -36,12 +40,17 @@ namespace Pokemon_API
                         case 3:
                             pokemon_url = $"https://pokeapi.co/api/v2/pokemon/3/";
                             break;
+                        case 4:
+                            pokemon_url = $"https://pokeapi.co/api/v2/pokemon/4";
+                            break;
                     }
-                    InvokeGet(pokemon_url);                  
+
+                    InvokeGet(pokemon_url);
                 }
 
-            } while (response > 3);
+            } while (response == 0 || response > 3);
         }
+
         public static void InvokeGet(string pokemon_url)
         {
             var client = new RestSharp.RestClient(pokemon_url);
@@ -50,13 +59,12 @@ namespace Pokemon_API
 
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
-                // Assuming response.Content is a JSON string
-                var jsonObject = Newtonsoft.Json.JsonConvert.DeserializeObject(response.Content);
 
-                // Serialize back to formatted JSON
-                string formattedJson = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObject, Newtonsoft.Json.Formatting.Indented);
+                // Parse my JSON object
+                var jsonObject = JsonSerializer.Deserialize<Mascot>(response.Content);
 
-                Console.WriteLine($"{response.Content}");
+                //Returns the formated string in the Mascot class
+                Console.WriteLine($"{jsonObject}".ToString());
             }
             else
             {
