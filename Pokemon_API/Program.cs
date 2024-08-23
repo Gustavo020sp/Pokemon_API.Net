74289bbe1aaa471ade0788a92b3887588f62f582
Gustavo Gomes <gustavo020sp@gmail.com>
2024-08-23 20:21:57 -0300

    Ajuste no Controller, adicionado sistema de injeção de dependência

--- Pokemon_API/Program.cs
+++ Pokemon_API/Program.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using Pokemon_API.Entities;
 using Microsoft.VisualBasic;
 using Pokemon_API.Controller;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Pokemon_API
 {
@@ -11,9 +12,24 @@ namespace Pokemon_API
     {
         static void Main(string[] args)
         {
-            TamagotchiController interaction = new TamagotchiController();
+            // Configura o contêiner DI
+            var serviceProvider = ConfigureServices();
 
-            interaction.GenerateInteraction();            
-        }        
+            // Resolve o controlador a partir do contêiner DI
+            var controller = serviceProvider.GetRequiredService<TamagotchiController>();
+
+            // Executa a lógica
+            controller.GenerateInteraction();
+        }
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // Registra os serviços e controladores
+            services.AddTransient<TamagotchiController>();
+            //services.AddTransient<UserInteraction>();
+
+            return services.BuildServiceProvider();
+        }
     }
 }
